e304c52cf837879e9011c21f61eaaa16f127b54f
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-13 01:23:45 -0800

    add-sidebar-templat-style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navigation from "./views/Navigation/Navigation";
 import Recommended from "./views/Recommended/Recommended";
 import Products from "./views/Products/Products";
+import Sidebar from "./views/Sidebar/Sidebar";
 function App() {
   return (
     <>
+      <Sidebar/>
       <Navigation />
       <Recommended />
       <Products />
