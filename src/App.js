47045eef33ab17ebd37a5115754964db92d5e2c7
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-13 22:36:30 -0800

    add-input-filter-logic

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import Recommended from "./views/Recommended/Recommended";
 import Products from "./views/Products/Products";
 import Sidebar from "./views/Sidebar/Sidebar";
 import { useState } from "react";
+import products from "./data/data"
 function App() {
+  const [SelectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
   
+  const handelInputChange = (event) => {
+   setQuery(event.target.value)
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+
   return (
     <>
       <Sidebar />
